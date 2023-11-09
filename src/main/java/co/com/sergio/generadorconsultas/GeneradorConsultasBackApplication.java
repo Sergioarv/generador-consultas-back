579142f612a849e7c9fced7388d80bb479fa5263
Sergio Rodriguez Vasquez <sarv9208@hotmail.com>
2023-11-09 09:10:51 -0500

    Primer commit creación de proyecto SpringBoot

---
+++ src/main/java/co/com/sergio/generadorconsultas/GeneradorConsultasBackApplication.java
@@ -0,0 +1,13 @@
+package co.com.sergio.generadorconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorConsultasBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorConsultasBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sergio/generadorconsultas/GeneradorConsultasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sergio.generadorconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneradorConsultasBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
