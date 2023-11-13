92a95b8e09a6377daa008cae114e0a4b3a9d9384
Sergio Rodriguez Vasquez <sarv9208@hotmail.com>
2023-11-13 15:00:15 -0500

    modificación de consultas con la api bigquery

--- src/main/java/co/com/sergio/generadorconsultas/controller/BigQueryController.java
+++ src/main/java/co/com/sergio/generadorconsultas/controller/BigQueryController.java
@@ -3,6 +3,10 @@ package co.com.sergio.generadorconsultas.controller;
 import co.com.sergio.generadorconsultas.dto.SchedulesDTO;
 import co.com.sergio.generadorconsultas.service.BigQueryService;
 import co.com.sergio.generadorconsultas.utils.GeneralResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,36 +34,43 @@ public class BigQueryController {
      * Método encargado de realizar las peticiones a BigQuery
      *
      * @param gameNumber, numero de juegos
-     * @param dayNight, D/N horario de dia o noche
-     * @param duration, duraccion en minutos y segundos con el formato (m:ss)
-     * @param status, estado del partido, closed, canceled o unnecessary
-     * @param year, año del patido
-     *
-     * @return, Lista de resultados "SchedulesDTO" los parametros recibidos
+     * @param dayNight,   D/N horario de dia o noche
+     * @param duration,   duraccion en minutos y segundos con el formato (m:ss)
+     * @param status,     estado del partido, closed, canceled o unnecessary
+     * @param year,       año del patido
+     * @param conditional, Condicional de busqueda, en caso de buscar mas de 1 parametros
+     *                     este condicional permite buscar ambos o almenos 1 de ellos
+     * @param pag, número de la página a buscar
+     * @param sizePag, cantidad de elementos por pagona
+     * @return Lista de resultados "SchedulesDTO" los parametros recibidos
      */
     @GetMapping
-    public ResponseEntity<GeneralResponse<List<SchedulesDTO>>> runQuery(
+    public ResponseEntity<GeneralResponse<Page<SchedulesDTO>>> runQuery(
             @RequestParam(value = "gameNumber", required = false) String gameNumber,
             @RequestParam(value = "dayNight", required = false) String dayNight,
             @RequestParam(value = "duration", required = false) String duration,
             @RequestParam(value = "status", required = false) String status,
-            @RequestParam(value = "year", required = false) String year
+            @RequestParam(value = "year", required = false) String year,
+            @RequestParam(value = "conditiaonal", defaultValue = "and", required = false) String conditional,
+            @RequestParam(value = "pag", defaultValue = "0", required = false) int pag,
+            @RequestParam(value = "sizePag", defaultValue = "10", required = false) int sizePag
     ) {
 
-        GeneralResponse<List<SchedulesDTO>> response = new GeneralResponse<>();
-        List<SchedulesDTO> data;
+        GeneralResponse<Page<SchedulesDTO>> response = new GeneralResponse<>();
+        Page<SchedulesDTO> data;
         HttpStatus httpStatus = HttpStatus.OK;
 
         try {
-            data = bigQueryService.runQuery(gameNumber);
+
+            data = bigQueryService.runQuery(gameNumber, dayNight, duration, status, year, conditional, pag, sizePag);
 
             if (data != null) {
                 response.setData(data);
                 response.setSuccess(true);
 
-                if (data.size() > 1) {
+                if (data.getContent().size() > 1) {
                     response.setMessage("Lista de horarios obtenida correctamente");
-                } else if (data.size() == 1) {
+                } else if (data.getContent().size() == 1) {
                     response.setMessage("horarios obtenida correctamente");
                 } else {
                     response.setSuccess(false);
--- src/main/java/co/com/sergio/generadorconsultas/service/BigQueryService.java
+++ src/main/java/co/com/sergio/generadorconsultas/service/BigQueryService.java
@@ -4,11 +4,9 @@ import co.com.sergio.generadorconsultas.dto.SchedulesDTO;
 import co.com.sergio.generadorconsultas.utils.ConvertTo;
 import com.google.cloud.bigquery.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,23 +27,57 @@ public class BigQueryService {
     }
 
 
-    public List<SchedulesDTO> runQuery(String sqlQuery) throws InterruptedException {
+    /**
+     * Método encargado de realizar las peticiones a BigQuery
+     *
+     * @param gameNumber,  numero de juegos
+     * @param dayNight,    D/N horario de dia o noche
+     * @param duration,    duraccion en minutos y segundos con el formato (m:ss)
+     * @param status,      estado del partido, closed, canceled o unnecessary
+     * @param year,        año del patido
+     * @param conditional, Condicional de busqueda, en caso de buscar mas de 1 parametros
+     *                     este condicional permite buscar ambos o almenos 1 de ellos
+     * @param pag,         número de la página a buscar
+     * @param sizePag,     cantidad de elementos por pagona
+     * @return Lista de resultados "SchedulesDTO" los parametros recibidos
+     * @throws InterruptedException, Si hay errores al ejecutar la consulta
+     */
+    public Page<SchedulesDTO> runQuery(String gameNumber, String dayNight, String duration, String status, String year, String conditional, int pag, int sizePag) throws InterruptedException {
 
         try {
-            //String encodedSqlQuery = URLEncoder.encode(sqlQuery, "UTF-8");
-            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(
-                    "SELECT * FROM bigquery-public-data.baseball.schedules where dayNight = 'N' LIMIT 10").build();
+
+            String sql = "SELECT * FROM bigquery-public-data.baseball.schedules";
+            StringBuilder params;
+
+            params = getParam(gameNumber, dayNight, duration, status, year, conditional);
+
+            if (!params.isEmpty()) {
+                sql += params;
+            }
+
+            // configuración y petición por medio del api
+            QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(sql).build();
             TableResult result = bigQuery.query(queryConfig);
 
+            // Lista de resultados de la api
             List<SchedulesDTO> resultList = new ArrayList<>();
             ConvertTo convertTo = new ConvertTo();
 
+            //se itera la lista para convertirla en SchedulesDTO
             for (FieldValueList fVL : result.iterateAll()) {
                 SchedulesDTO schedulesDTO = convertTo.convertFieldValueListToSchedulesDTO(fVL);
                 resultList.add(schedulesDTO);
             }
 
-            return resultList;
+            // Se genera la paginación de la lista resultante
+            Pageable pageable = PageRequest.of(pag, sizePag, Sort.by("gameId").descending());
+            int start = (int) pageable.getOffset();
+            int end = Math.min((start + pageable.getPageSize()), resultList.size());
+
+            // Se determina el tamaño de la lista paginada y se seprada una sublista
+            List<SchedulesDTO> resultListContent = resultList.subList(start, end);
+            // Se genera y retorna la paginación con la sublista
+            return new PageImpl<>(resultListContent, pageable, resultList.size());
 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -54,8 +86,81 @@ public class BigQueryService {
             // Manejar las excepciones específicas de BigQuery
             throw new RuntimeException("Error de BigQuery: " + e.getMessage(), e);
         }
-//        } catch (UnsupportedEncodingException e) {
-//            throw new RuntimeException("No soporta caracteres: " + e.getMessage(), e);
-//        }
+    }
+
+    /**
+     * Método encargado de verificar cada paramtro de busqueda y generar un string
+     * con la estructura de la query
+     *
+     * @param gameNumber,  numero de juegos
+     * @param dayNight,    D/N horario de dia o noche
+     * @param duration,    duraccion en minutos y segundos con el formato (m:ss)
+     * @param status,      estado del partido, closed, canceled o unnecessary
+     * @param year,        año del patido
+     * @param conditional, Condicional de busqueda, en caso de buscar mas de 1 parametros
+     *                     este condicional permite buscar ambos o almenos 1 de ellos
+     * @return un string con la estructura de la query segun los parametros buscados
+     */
+    private StringBuilder getParam(String gameNumber, String dayNight, String duration, String status, String year, String conditional) {
+
+        StringBuilder params = new StringBuilder();
+
+        // verifica si se busca por parametro gamenumber
+        if (gameNumber != null && !gameNumber.isEmpty()) {
+            //verifica si ya hay parametros de busqueda
+            // Si no hay genera la estructura con un where y el parametro
+            // en caso contrario la estructura es el condicional y el parametro
+            if (params.isEmpty()) {
+                params.append(" where gameNumber = ").append(gameNumber);
+            } else {
+                params.append(" ").append(conditional).append(" gameNumber = ").append(gameNumber);
+            }
+        }
+// verifica si se busca por parametro dayNight
+        if (dayNight != null && !dayNight.isEmpty()) {
+            //verifica si ya hay parametros de busqueda
+            // Si no hay genera la estructura con un where y el parametro
+            // en caso contrario la estructura es el condicional y el parametro
+            if (params.isEmpty()) {
+                params.append(" where dayNight = '").append(dayNight).append("'");
+            } else {
+                params.append(" ").append(conditional).append(" dayNight = '").append(dayNight).append("'");
+            }
+        }
+        // verifica si se busca por parametro duration
+        if (duration != null && !duration.isEmpty()) {
+            //verifica si ya hay parametros de busqueda
+            // Si no hay genera la estructura con un where y el parametro
+            // en caso contrario la estructura es el condicional y el parametro
+            if (params.isEmpty()) {
+                params.append(" where duration = '").append(duration).append("'");
+            } else {
+                params.append(" ").append(conditional).append(" duration = '").append(duration).append("'");
+            }
+        }
+// verifica si se busca por parametro status
+        if (status != null && !status.isEmpty()) {
+            //verifica si ya hay parametros de busqueda
+            // Si no hay genera la estructura con un where y el parametro
+            // en caso contrario la estructura es el condicional y el parametro
+            if (params.isEmpty()) {
+                params.append(" where status = '").append(status).append("'");
+            } else {
+                params.append(" ").append(conditional).append(" status = '").append(status).append("'");
+            }
+        }
+// verifica si se busca por parametro year
+        if (year != null && !year.isEmpty()) {
+            //verifica si ya hay parametros de busqueda
+            // Si no hay genera la estructura con un where y el parametro
+            // en caso contrario la estructura es el condicional y el parametro
+            if (params.isEmpty()) {
+                params.append(" where year = ").append(year);
+            } else {
+                params.append(" ").append(conditional).append(" year = ").append(year);
+            }
+        }
+
+        return params;
     }
 }
